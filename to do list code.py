540676eaf5290cea2b8a512e6d3b506b0ba695fb
Starship87 <55468638+Starship87@users.noreply.github.com>
2019-10-22 19:29:09 -0500

    Add files via upload

--- to do list code.py
+++ to do list code.py
@@ -1,7 +1,7 @@
 import sys
 
-todolist = {}
-next_key = 1
+todolist = []
+
 def menu():
     print("choose your option.")
     print("7 = delete item")
@@ -23,7 +23,6 @@ def menu():
     
 
 def add():
-    global next_key
     print("add function")
     todo_item = ""
     while todo_item == "":
@@ -32,27 +31,74 @@ def add():
     print("when is it due")
     due_date = input ()
 
-    todolist[next_key] = [todo_item, due_date]
+    todolist.append([todo_item, due_date])
+    save_data()
     show_list()
+    
     menu()
 
 def delete():
     print("")
     show_list()
     print("which item number do you want ot delete?")
-    delete_me = int(input())
-    if delete_me in todolist:
-        del todolist[delete_me]
+    delete_me = int(input()) - 1
+    if delete_me <= len (todolist):
+        todolist.pop(delete_me)
+        save_data()
     else:
         print("item not found")
     menu()
+    
 def show_list():
-    print(todolist)
+    for i in range(len(todolist)):
+        print(str(i+1) + ":"+ todolist[i][0]+" Due: "+ todolist[i][1])
+        
+        
+
+def save_data():
+    global todolist
+
+    try:
+        myfile = open("todolist.txt", "w")
+        #create a string out of our list
+        s = ''
+        for item in todolist:
+            s = s + ','.join(item) + ";"
+        s = s 
+       
+
+        myfile.write(s)
+        myfile.close()
+    except:
+        print("File not updated")
 
 
+    
+def load_data():
+    global todolist
+
+    try:
+        myfile = open ("todolist.txt", "r")
+        mytext = myfile.read()
+        myfile.close()
+
+        #this is where we make the data into our data list
+        #li = list(string.split(""))
+        print(mytext)
+        print(mytext.split(";"))
+        print(firstlist)
+        firstlist.pop()
+        todolist = []
+        for item in firstlist:
+            todolist.append (item.split(","))
+        print(todolist)
+        
+    except:
+        print ("File not read")
 def quit():
     sys.exit()
-  
+
+load_data()
 menu()
 
     
